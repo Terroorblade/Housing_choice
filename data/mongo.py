7e86d398c36ce86508b1728d6ee92e4dceb87987
Terroor <101572196+Terroorblade@users.noreply.github.com>
2026-06-05 18:28:33 +0300

    подключение к монго атлас

--- data/mongo.py
+++ data/mongo.py
@@ -1,11 +1,26 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+# client = MongoClient("mongodb://localhost:27017/")
+# # client = MongoClient("mongodb+srv://admin:5598v1234!@houschoice.eysbain.mongodb.net/?appName=Houschoice")
+
+# db = client["vkr_apartments"]
+
+# apartments_collection = db["apartments"]
+
+# districts_collection = db["districts"]
+
+# saved_surveys_collection = db["saved_surveys"]
+
+
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.environ["MONGO_URI"]
+
+client = MongoClient(MONGO_URI)
 
 db = client["vkr_apartments"]
 
 apartments_collection = db["apartments"]
-
 districts_collection = db["districts"]
-
 saved_surveys_collection = db["saved_surveys"]
